a752df502ffe4865c098ea52badfd8344fd9efb6
almathaler <athaler00@stuy.edu>
2019-01-03 21:58:12 -0500

    main method basicccss

--- Ciphers.java
+++ Ciphers.java
@@ -1,5 +1,14 @@
 public class Ciphers {
   public static void main(String[] args) {
-    //comment to see if ik how to use git
+    String typeCipher = args[0];
+    typeCipher cipherObject = new typeCipher();
+    String text = args[1];
+    if (args[2].equals("encrypt")){
+      System.out.println("Your encrypted plaintext: " + cipherObject.encrypt(text));
+    }else if (args[2].equals("decrypt")){
+      System.out.println("Your decrypted text: " + cipherObject.decrypt(text));
+    }else{
+      System.out.println("Please specify whether you would like to encrypt or decrypt your text.");
+    }
   }
 }
