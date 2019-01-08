f1a4638825843bc08a6eca745b2fdfed9d496f12
DarthChurros <tnarayan00@stuy.edu>
2019-01-08 14:52:51 -0500

    fixed constructorsgit add Cipher.java Ciphers.java PolySubCipher.java TableCipher.java !

--- PolySubCipher.java
+++ PolySubCipher.java
@@ -2,5 +2,20 @@ public abstract class PolySubCipher extends Cipher{
   private char[][] keyGrid;
   private int counter;
 
+  public PolySubCipher(int keyNum, String keyStr) {
+    super(keyNum, keyStr);
+    counter = 0;
+  }
+
+  protected abstract void fillGrid();
+
   protected abstract char encryptChar(char plain);
+
+  protected char[][] getGrid() {
+    return keyGrid;
+  }
+
+  protected int counter() {
+    return counter;
+  }
 }
