447bc66a2bfec4151103b0974cca8bcdd15076a7
almathaler <athaler00@stuy.edu>
2019-01-09 14:51:06 -0500

    done

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,47 +1,28 @@
 import java.lang.Math;
 import java.lang.IllegalStateException;
-public class CaesarCipher extends PolySubCipher{
+public class CaesarCipher extends SubCipher{
+  /*
+  public static void main(String[] args){
+    CaesarCipher sc = new CaesarCipher(Integer.parseInt(args[0]));
+    System.out.println("Encrypted message: " + sc.encrypt(args[1]));
+    System.out.println("Decrypted message: " + sc.decrypt(sc.encrypt(args[1])));
+    System.out.println("Testing if key works twice in a row: ");
+    CaesarCipher sc1 = new CaesarCipher(Integer.parseInt(args[0]));
+    System.out.println("Encrypted message: " + sc1.encrypt(args[1]));
+    System.out.println("Decrypted message: " + sc1.decrypt(sc1.encrypt(args[1])));
+    System.out.println("Testing if key works twice in a row: ");
+  }
+  */
   public CaesarCipher(int k){
-    super(k, ""); //k is the shift amount
-    fillGrid();
+    super(k); //k is the shift amount
   }
-  public String encrypt(String plaintext){
-    plaintext = plaintext.toUpperCase();
-    //convert charAts to ascii #s, -65 so to get range from 0 to 25. these indexes are
-    //what the new letters will be, as per the keyGrid
-    String toReturn = "";
-    for (int i = 0; i<plaintext.length(); i++){
-      toReturn+=encryptChar(plaintext.charAt(i));
-    }
-    return toReturn;
-  }
-
-  public String decrypt(String ciphertext){ //this was is runtime n2, but works for random key
-    ciphertext = ciphertext.toUpperCase(); //maybe process this in public static void main(String[] args) {
-    String toReturn = "";
-    for (int i = 0; i<ciphertext.length(); i++){
-      char toAdd = 'A';
-      for (int j = 0; j<26; j++){
-        if (getGrid()[0][j] == ciphertext.charAt(i)){
-          toAdd += j; //remember, j is position of original letter in alphabet
-        }
-      }
-      toReturn += (char) toAdd;
-    }
-    return toReturn;
-  }
-
-  protected char encryptChar(char c){
-    int index = (int) c - 65;
-    return getGrid[0][index];
-  }
-
-  protected void fillGrid(){
+  protected char[][] genGrid(){
     if (getGrid() == null){
-      getGrid() = new char[1][26]; //getGrid returns reference to array itself, so you have access to modifying it
+      char[][] grid = new char[1][26];
       for (int i = 0; i<26; i++){
-        getGrid()[0][i] = (char) ('A' + (i+getKey())%26); //can i use get grid here
+        grid[0][i] = (char) ('A' + (i+getKey())%26); //can i use get grid here
       }
+      return grid;
     }else{
       throw new IllegalStateException("You can't call fillGrid twice!");
     }
