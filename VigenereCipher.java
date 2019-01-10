deb4733d3f77c01d958accb24dac6eb9d083c435
DarthChurros <tnarayan00@stuy.edu>
2019-01-10 00:29:18 -0500

    I think I'm done

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class VigenereCipher extends PolySubCipher {
 
   public VigenereCipher(String keyStr) {
@@ -11,8 +12,30 @@ public class VigenereCipher extends PolySubCipher {
     char[][] grid = new char[getKeyword().length()][26];
     for (int i = 0; i < grid.length; i++) {
       for (int j = 0; j < 26; j++) {
-        grid[i][j] = (char)((j + getKeyword().charAt(i) - 'a') % 26 + 'A');
+        grid[i][j] = (j + Character.toUpperCase(getKeyword().charAt(i)) - 'A') % 26 + 'A';
       }
     }
+  }
+
+  protected char encryptChar(char plain) {
+    iter();
+    return getGrid()[counter()][Character.toUpperCase(plain) - 'A'];
+  }
+
+  public String encrypt(String plaintext) {
+    reset();
+    String ciphertext = "";
+    for(int i = 0; i < plaintext.length(); i++) {
+      ciphertext += encryptChar(plaintext.charAt(i));
+    }
+  }
+
+  public String decrypt(String ciphertext) {
+    reset();
+    String plaintext = "";
+    for (int i = 0; i < ciphertext.length(); i++) {
+        plaintext += Arrays.indexOf(getGrid()[counter()], Character.toUpperCase(ciphertext.charAt(i)));
+    }
+    return plaintext.toUpperCase();
   }
 }
