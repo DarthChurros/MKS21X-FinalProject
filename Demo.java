67fe5fd392f39dab0d1fbfafd8cf36520228053e
DarthChurros <tnarayan00@stuy.edu>
2019-01-10 14:51:13 -0500

    started on main, wrote selection screen

---
+++ Demo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Demo {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    Cipher util;
+    System.out.println("Welcome to the Wii Fit Cipher Service! To begin, please select a cipher: \n");
+    System.out.println("\t[1] Substitution Cipher");
+    System.out.println("\t[2] Caesar Cipher");
+    System.out.println("\t[3] Vigenere Cipher\n");
+
+    switch (in.next()) {
+      
+    }
+  }
+}
